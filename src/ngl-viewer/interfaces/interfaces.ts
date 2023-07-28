25d1f1b2e3a72d2762783296775088e2bd2b7adc
Jonathan Willianto <jowillianto@gmail.com>
2023-07-29 04:23:53 +0900

    deleted unused files

--- src/ngl-viewer/interfaces/interfaces.ts
+++
@@ -1,6 +0,0 @@
-type ViewSetting = {
-  type    : string,
-  params: { [key: string]: number }; 
-}
-
-export type ViewSettings = Array<ViewSetting>
--- src/ui/interface.ts
+++
@@ -1,5 +0,0 @@
-export type ComponentViewParams = {
-  type    : string 
-  params  : Record<string, any>
-  visible : boolean
-}
