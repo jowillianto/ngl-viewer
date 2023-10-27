f24ea265de7cb8428e50d92926e1fba10207d112
Nuzup Shadiev <nuzupshadiev@Nuzups-MacBook-Pro.local>
2023-10-27 14:14:09 +0900

    add customization for light and dark color

--- src/ngl-viewer/user-interface/theme-change.tsx
+++ src/ngl-viewer/user-interface/theme-change.tsx
@@ -2,17 +2,25 @@ import React, { useContext, useState } from "react";
 import StageContext from "../stage-context";
 
 export type ThemeSwitcherProps<T> = {
-  render: React.ComponentType<{ onClick: () => void; currentTheme: string } & T>;
+  render: React.ComponentType<{ 
+    onClick: () => void; currentTheme: string 
+  } & T>;
+  lightTheme?: string;
+  darkTheme? : string
 };
 
-const ThemeSwitcher = <T,>({ render }: ThemeSwitcherProps<T>) => {
+const ThemeSwitcher = <T,>({ 
+  render, lightTheme = 'white', darkTheme = 'black'
+}: ThemeSwitcherProps<T>) => {
   const [currentTheme, setCurrentTheme] = useState<string>('dark'); 
   const { stage } = useContext(StageContext);
 
   const toggleTheme = () => {
     const newTheme = currentTheme === 'light' ? 'dark' : 'light';
     setCurrentTheme(newTheme);
-    newTheme === 'light' ? stage?.setParameters({ backgroundColor: 'white' }) : stage?.setParameters({ backgroundColor: 'black' });
+    newTheme === 'light' ? 
+      stage?.setParameters({ backgroundColor: lightTheme }) : 
+      stage?.setParameters({ backgroundColor: darkTheme });
   };
 
   const Component = render;
