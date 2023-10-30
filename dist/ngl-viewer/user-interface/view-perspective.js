1a359923b21e6cef4e45b793c766960a6beff838
Nuzup Shadiev <nuzupshadiev@Nuzups-MacBook-Pro.local>
2023-10-30 11:11:03 +0900

    SetCameraType gives an onclick that takes a perspective argument

--- dist/ngl-viewer/user-interface/view-perspective.js
+++ dist/ngl-viewer/user-interface/view-perspective.js
@@ -10,19 +10,17 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 import { jsx as _jsx } from "react/jsx-runtime";
-import React, { useContext } from "react";
+import { useContext } from "react";
 import StageContext from "../stage-context";
 var SetCameraType = function (_a) {
-    var render = _a.render, type = _a.type;
+    var render = _a.render;
     var stage = useContext(StageContext).stage;
-    var _b = React.useState(type), cameraType = _b[0], setCameraType = _b[1];
     var updateCameraType = function (newCameraType) {
-        setCameraType(newCameraType);
         stage === null || stage === void 0 ? void 0 : stage.setParameters({ cameraType: newCameraType });
     };
     var Component = render;
     var renderProps = {
-        onClick: function () { return updateCameraType(cameraType); },
+        onClick: function (cameraType) { return updateCameraType(cameraType); },
     };
     return _jsx(Component, __assign({}, renderProps));
 };
