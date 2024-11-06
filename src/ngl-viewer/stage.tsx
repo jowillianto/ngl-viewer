65a5be8c498756a7163ca3390f7581c396956066
Jonathan Willianto <jowillianto@gmail.com>
2024-11-06 13:00:20 +0900

    [#main] fix?

--- src/ngl-viewer/stage.tsx
+++ src/ngl-viewer/stage.tsx
@@ -161,9 +161,9 @@ export default function Stage({
       .addArrow([0, 0, 0], [5, 0, 0], hexToRgb(colorX) || [255, 0, 0], 0.5, "X")
       .addArrow([0, 0, 0], [0, 5, 0], hexToRgb(colorY) || [0, 255, 0], 0.5, "Y")
       .addArrow([0, 0, 0], [0, 0, 5], hexToRgb(colorZ) || [0, 0, 255], 0.5, "Z")
-      .addText([5, 0, 0], invertBgColor, 4, "X")
-      .addText([0, 5, 0], invertBgColor, 4, "Y")
-      .addText([0, 0, 5], invertBgColor, 4, "Z");
+      .addText([5, 0, 0], invertBgColor.clone(), 4, "X")
+      .addText([0, 5, 0], invertBgColor.clone(), 4, "Y")
+      .addText([0, 0, 5], invertBgColor.clone(), 4, "Z");
     const component = miniStage.addComponentFromObject(shape);
     component?.addRepresentation("buffer", { opacity: 1 });
   }, [miniStage, colorX, colorY, colorZ]);
