e7f3dfb0bad5a2544bf17bca0d131623dcdac575
Jonathan Willianto <jowillianto@gmail.com>
2025-02-11 11:11:00 +0900

    [#dev] remove import

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,8 @@ import ViewerPanel from "./ngl-viewer/user-interface/viewer-panel";
 import ViewerStage from "./ngl-viewer/user-interface/viewer-stage";
 import ViewerSelector from "./ngl-viewer/user-interface/component-selector";
 import { useStage } from "./ngl-viewer/stage-context";
-import { useComponentFromObject } from "./ngl-viewer/shapes/base-shape";
 import useComponent from "./ngl-viewer/shapes/base-shape";
 
-export {
-  ViewerPanel,
-  ViewerStage,
-  ViewerSelector,
-  useStage,
-  useComponent,
-  useComponentFromObject,
-};
+export { ViewerPanel, ViewerStage, ViewerSelector, useStage, useComponent };
 
 export default ProteinViewer;
