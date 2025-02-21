1b6f3c9380b2dbf20d7700983374975220505485
Jonathan Willianto <jowillianto@gmail.com>
2025-02-21 13:28:29 +0900

    [#main] Fix lifecycle controller

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,31 @@ import ProteinViewer from "./ngl-viewer/user-interface/protein-viewer";
 import FileComponent from "./ngl-viewer/file/file";
 import ViewerStage from "./ngl-viewer/user-interface/viewer-stage";
 import NGLBox from "./ngl-viewer/shapes/box";
+
+const containerStyles = {
+  width: "100%",
+  height: "100%",
+};
+
+const stageSettings = {
+  backgroundColor: "white",
+};
 const App = () => {
   const [toggle, setToggle] = React.useState(true);
+  console.log(toggle)
   return (
     <React.StrictMode>
       <button onClick={() => setToggle(!toggle)}>Toggle</button>
-      <div style={{ backgroundColor: "grey", height: `${window.innerHeight - 32}px` }}>
+      <div
+        style={{
+          backgroundColor: "grey",
+          height: `${window.innerHeight - 32}px`,
+        }}
+      >
         <ProteinViewer>
           <ViewerStage
-            containerStyles={{
-              width: "100%",
-              height: "100%",
-            }}
-            viewSettings={{ backgroundColor: toggle ? "black" : "white" }}
+            containerStyles={containerStyles}
+            viewSettings={stageSettings}
           />
           {/* <ViewerPanel /> */}
           <FileComponent
